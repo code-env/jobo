336815a6b4487eef6d0e39eb31addce796556994
Romaric250 <lonfonyuyromeo@gmail.com>
2024-07-08 02:13:35 +0100

    some updates done on updating the submitsion functionality

--- app/api/user/applyforhack/[hackId]/stage/[stageId]/route.ts
+++ app/api/user/applyforhack/[hackId]/stage/[stageId]/route.ts
@@ -86,3 +86,67 @@ export async function POST(
     return new NextResponse("Internal Server error", { status: 500 });
   }
 }
+
+export async function GET(
+  req: Request,
+  { params }: { params: { hackId: string; stageId: string } }
+) {
+  try {
+    const user = await currentUser();
+    const { hackId } = params;
+    if (!hackId) {
+      return new NextResponse("HackId is required", { status: 400 });
+    }
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 400 });
+    }
+
+    const userdata = await db.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+
+    if (!userdata) {
+      return new NextResponse("No user found", { status: 404 });
+    }
+
+    const hackData = await db.postHack.findUnique({
+      where: {
+        id: hackId,
+      },
+    });
+
+    if (!hackData) {
+      return new NextResponse("No hack found", { status: 404 });
+    }
+
+    const doesstageexist = await db.stage.findUnique({
+      where: {
+        id: params.stageId,
+      },
+    });
+    if (!doesstageexist) {
+      return new NextResponse("No stage found", { status: 404 });
+    }
+
+    const applydata = await db.submittions.findFirst({
+      where: {
+        userId: userdata.id,
+        postId: hackId,
+        stageId: params.stageId,
+      },
+    });
+
+    if (!applydata) {
+      return new NextResponse("No application found for this stage", {
+        status: 404,
+      });
+    }
+
+    return new NextResponse(JSON.stringify(applydata), { status: 200 });
+  } catch (error: any) {
+    console.log(error.message);
+    return new NextResponse("Internal Server error", { status: 500 });
+  }
+}
