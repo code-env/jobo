8fe09fbf3487cff49c01375a9d25c6f84168c2d5
Romaric250 <lonfonyuyromeo@gmail.com>
2024-07-08 19:13:32 +0100

    project completion done

--- app/(after-auth)/(after-setting-user)/inbox/page.tsx
+++ app/(after-auth)/(after-setting-user)/inbox/page.tsx
@@ -1,12 +1,24 @@
 import { Metadata } from "next";
 import React from "react";
+import TextMorph from "@/components/forms/text-morph";
+import { currentUser } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: "Inbox",
 };
 
-const Inbox = () => {
-  return <div>Inbox</div>;
+const Inbox = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div>
+      <TextMorph params={{ conversationId: user.id }} />
+    </div>
+  );
 };
 
 export default Inbox;
