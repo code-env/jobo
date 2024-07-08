8d216fcabd59d96524cab9649c09e08d34e12086
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-07-08 18:33:43 +0000

    feat some task done

---
+++ app/(after-auth)/(after-setting-user)/inbox/[conversationId]/page.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from "next";
+import React from "react";
+import TextMorph from "@/components/forms/text-morph";
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export const metadata: Metadata = {
+  title: "Inbox",
+};
+
+const Inbox = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+  const userdata = await db.user.findUnique({
+    where:{
+        clerkId:user.id
+    }
+  })
+
+  if(!userdata) return 
+
+  if (userdata)
+
+  return (
+    <div>
+      <TextMorph params={{ conversationId: user.id }} />
+    </div>
+  );
+};
+
+export default Inbox;
