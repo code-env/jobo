28ccb94723be2883360817e06abe804021f4761f
code-env <org.codeen@gmail.com>
2024-07-08 03:41:32 +0100

    feat: protected the other pages

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/d(.*)", "user-type", "skills"]);
+const isProtectedRoute = createRouteMatcher([
+  "/d(.*)",
+  "/user-type",
+  "/skills",
+  "/settings",
+  "/inbox(.*)",
+  "/showcase",
+  "/search",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
