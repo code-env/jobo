87686d384e1e9b0bad1a0d3c0c83e48733197256
code-env <org.codeen@gmail.com>
2024-07-09 08:57:59 +0100

    feat: nothing

--- app/(after-auth)/(after-setting-user)/scroll/[scrollId]/page.tsx
+++ app/(after-auth)/(after-setting-user)/scroll/[scrollId]/page.tsx
@@ -1,9 +1,82 @@
+import SinglePost from "@/components/shared/single-post";
+import { db } from "@/lib/db";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React from "react";
 
 const ScrollPost = async ({ params }: { params: { scrollId: string } }) => {
-  console.log(params.scrollId);
+  const post = await db.showCasePost.findUnique({
+    where: {
+      id: params.scrollId,
+    },
+    select: {
+      Comments: true,
+      likescount: true,
+      images: true,
+      title: true,
+      description: true,
+      createdAt: true,
+      id: true,
+      user: true,
+    },
+  });
 
-  return <div>ScrollPost</div>;
+  if (!post) return <div className="children center">Post not found</div>;
+
+  return (
+    <div className="children">
+      <div className="page border-b border-border flex gap-3">
+        <div className="w-10 h-10 min-w-10 relative rounded-full overflow-hidden">
+          <Image
+            fill
+            alt={`jobo user ${post?.user.username}`}
+            src={post.user.profilePicture!}
+          />
+        </div>
+        <div>
+          <p className="flex flex-col">
+            {post.user?.username}{" "}
+            <span className="text-xs text-neutral-400">
+              {new Date(post.createdAt).toLocaleDateString()}
+            </span>
+          </p>
+          <h2 className="font-semibold text-3xl">{post.title}</h2>
+
+          <p className="mt-2 mb-2">{post.description}</p>
+
+          <div className="flex w-full flex-col relative">
+            {post.images?.map((image, index) => {
+              //some code here
+              return (
+                <div
+                  key={index}
+                  className={cn(
+                    "h-96 w-full overflow-hidden  border rounded-lg relative"
+                  )}
+                >
+                  <Image
+                    src={image}
+                    alt="Nothing for now"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <SinglePost
+        Comments={post.Comments}
+        description={post.description}
+        images={post.images}
+        id={post.id}
+        likeCount={post.likescount}
+        title={post.title}
+        user={post.user}
+      />
+    </div>
+  );
 };
 
 export default ScrollPost;
---
+++ components/shared/single-post.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Comments, Likes, User } from "@prisma/client";
+import axios from "axios";
+import { revalidatePath } from "next/cache";
+import Image from "next/image";
+import React, { FormEvent, useState } from "react";
+
+export type Post = {
+  id: string;
+  title: string;
+  description: string;
+  Comments: Comments[];
+  user: User;
+  likeCount: number;
+  images: string[];
+};
+
+const SinglePost = ({ id, title, description, Comments, user }: Post) => {
+  const [text, setText] = useState("");
+  const [likes, setLikes] = useState<Likes[] | null>(null);
+  const [comments, setComments] = useState<Comments[] | null>(null);
+  const [isLoading, setisLoading] = useState(false);
+
+  const handleSubmitComments = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      setisLoading(true);
+
+      const { data } = await axios.post(`/api/user/showcase/${id}/comments`, {
+        message: text,
+      });
+
+      setComments([...(comments as Comments[]), data]);
+
+      setText("");
+
+      revalidatePath("/");
+    } catch (error: any) {
+      console.log(error.message);
+    } finally {
+      setisLoading(false);
+    }
+  };
+
+  return (
+    <div className="">
+      <div className="page border-border border-b">
+        <form
+          className="flex items-center gap-3"
+          onSubmit={handleSubmitComments}
+        >
+          <input
+            placeholder="Comment here..."
+            className={cn(
+              "h-10 w-full border border-border rounded-lg outline-none px-5",
+              {
+                "opacity-50 cursor-not-allowed": isLoading,
+              }
+            )}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button
+            className={cn(
+              "bg-primary px-3 py-1.5 text-white rounded-lg font-semibold capitalize",
+              {
+                "opacity-50 cursor-not-allowed": isLoading,
+              }
+            )}
+          >
+            send
+          </button>
+        </form>
+      </div>
+      <div className="page border-border border-b">
+        {Comments.map((comment) => (
+          <div className="flex gap-3 py-2" key={comment.id}>
+            <div className="w-10 h-10 min-w-10 relative rounded-full overflow-hidden">
+              <Image
+                fill
+                alt={`jobo user ${user?.username}`}
+                src={user?.profilePicture!}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <p className="flex flex-col">{user?.username}</p>
+              <p className="">{comment.message}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SinglePost;
